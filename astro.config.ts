c65cc40b807dc21c46a234543e6edff4e54d3edf
Artrointel <artrointel@gmail.com>
2025-12-23 16:29:17 +0900

    Publish gh io page

--- astro.config.ts
+++ astro.config.ts
@@ -23,6 +23,8 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
 
 export default defineConfig({
   output: 'static',
+  site: 'https://metaintelligence.github.io',
+  base: '/metaintelligence',
 
   integrations: [
     tailwind({
